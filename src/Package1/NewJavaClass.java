d3504c90e5b3f99e2069c5b46d0d371abcf2d9bd
maneesh_patel <manishpatel1985@googlemail.com>
2020-11-16 16:18:36 +0000

    Hello Second commit

--- src/Package1/NewJavaClass.java
+++ src/Package1/NewJavaClass.java
@@ -3,5 +3,7 @@ package Package1;
 public class NewJavaClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Happy new Year");
+        System.out.println("Hello to 2 nd commit");
     }
 }
