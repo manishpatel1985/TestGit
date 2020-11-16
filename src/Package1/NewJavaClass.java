73bd16b67aa05098a128b6d9338115d54ece137b
maneesh_patel <manishpatel1985@googlemail.com>
2020-11-16 16:27:32 +0000

    commit1
    # Conflicts:
    #	src/Package1/NewJavaClass.java

--- src/Package1/NewJavaClass.java
+++ src/Package1/NewJavaClass.java
@@ -3,5 +3,6 @@ package Package1;
 public class NewJavaClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Happy New year");
     }
 }
